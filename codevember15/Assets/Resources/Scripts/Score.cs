edc83b0326ee5d3485e62e0e3f8fcd68eff112b2
Ido Freeman <idofriman@gmail.com>
2015-11-21 18:54:05 +0100

    score

---
+++ codevember15/Assets/Resources/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        gameObject.SetActive(false);
+	}
+    /**
+    update score with 1 point according to game speed
+    **/
+    void updateScroe(int speed) {
+        double factor = speed / 100;
+        int newScore = (int) Math.Round(factor);
+
+        score += newScore;
+        GameObject.Find("Score").GetComponent<Text>().text = score.ToString();
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
