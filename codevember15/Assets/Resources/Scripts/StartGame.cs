a136c56907685135b849fa78ccda2625911226d2
Patrick Schulz <Schulz@Macbook911.fritz.box>
2015-11-21 22:17:26 +0100

    added start animation, removed first circle double

--- codevember15/Assets/Resources/Scripts/StartGame.cs
+++ codevember15/Assets/Resources/Scripts/StartGame.cs
@@ -5,14 +5,33 @@ public class StartGame : MonoBehaviour {
 
 	public GameObject menu;
 	public GameObject game_manager;
+	GameObject buttonAnimated;
+	bool activated;
+
+	void Start() {
+		
+		buttonAnimated.GetComponent<Animator>().enabled = false;
+	}
+
+	// animation
+	IEnumerator WaitForAnimation() {
+
+		if(!activated) {
+			buttonAnimated.GetComponent<Animator>().enabled = true;
+			buttonAnimated.GetComponent<Animator>().SetTime(0.0f);
+			buttonAnimated.GetComponent<Animator>().cullingMode = AnimatorCullingMode.AlwaysAnimate;
+			
+			yield return new WaitForSeconds(1);
+			game_manager.GetComponent<GameLogic>().setRunning();
+		}
+		menu.SetActive(activated);
+	}
 
 	// starts the game if active = false
 	public void Run(bool active) {
 
-		menu.SetActive(active);
-		if(!active) {
-			game_manager.GetComponent<GameLogic>().spawnCircle();
-			game_manager.GetComponent<GameLogic>().setRunning();
-		}
+		activated = active;
+		buttonAnimated = GameObject.Find("StartButton");
+		StartCoroutine(WaitForAnimation());
 	}
 }
