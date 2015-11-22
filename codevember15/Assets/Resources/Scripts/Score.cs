7963d130cf42c1518d7969a2feec9abc64486b54
schlenger <mat.schlenker@gmail.com>
2015-11-22 18:57:55 +0100

    #fixed score savings and android settings

--- codevember15/Assets/Resources/Scripts/Score.cs
+++ codevember15/Assets/Resources/Scripts/Score.cs
@@ -15,7 +15,7 @@ public class Score : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		score = 0;
-        bestScore = 0;
+        bestScore = PlayerPrefs.GetInt("Highscore");
 		GameObject.Find("Score").GetComponent<Text>().text = "Score: " + score.ToString();
 	}
 
@@ -30,69 +30,73 @@ public class Score : MonoBehaviour {
     }
 
     public void onGameOver() {
-        Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-        List<ScoreRow> highscoreTable = new List<ScoreRow>();
-        System.IO.StreamReader file;
-        // open the scoring table file
-        try
-        {
-             file = new System.IO.StreamReader(file_name);
-        }
-        // create table if doesnt exist
-        catch (IOException e)
-        {
-            Debug.Log("Writing a new highscore table");
-            highscoreTable.Add(new ScoreRow(0, score, unixTimestamp.ToString()));
-            writeScoreTable(highscoreTable);
-            return;
-        }
-
-        // load the scoring table from file
-        string[] substrings;
-
-        string fileStr = file.ReadToEnd();
-        string[] txtlines = fileStr.Split(new char[] { '\n' });
-        file.Close();
-
-        for (int i = 0; i < txtlines.Length && highscoreTable.Count < 5; i++) {
-            if (txtlines[i] == "") {
-                continue;
-            }
-
-            substrings = Regex.Split(txtlines[i], " ");
-            ScoreRow row = new ScoreRow(i, int.Parse(substrings[1]), substrings[0]);
-
-            if (i==0)
-            {
-                bestScore = int.Parse(substrings[1]);
-            }
-
-            if (row.isBetter(score))
-            {
-                if (highscoreTable.Count < 4)
-                {
-                    if (i == 0)
-                    {
-                        bestScore = score;
-                    }
-                    highscoreTable.Add(new ScoreRow(i, score, unixTimestamp.ToString()));
-                    row.id++;
-                    highscoreTable.Add(row);
-                } else {
-                    row.updateRow(score, unixTimestamp.ToString());
-                    highscoreTable.Add(row);
-                }
-            } else {
-                highscoreTable.Add(row);
-            }
-        }
-
-        if (highscoreTable.Count < 5) {
-            highscoreTable.Add(new ScoreRow(highscoreTable.Count, score, unixTimestamp.ToString()));
-        }
-
-        // update list is necessary
-        writeScoreTable(highscoreTable);
+		if (score > bestScore) {
+			PlayerPrefs.SetInt ("Highscore", score);
+			bestScore = score;
+		}
+//        Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+//        List<ScoreRow> highscoreTable = new List<ScoreRow>();
+//        System.IO.StreamReader file;
+//        // open the scoring table file
+//        try
+//        {
+//             file = new System.IO.StreamReader(file_name);
+//        }
+//        // create table if doesnt exist
+//        catch (IOException e)
+//        {
+//            Debug.Log("Writing a new highscore table");
+//            highscoreTable.Add(new ScoreRow(0, score, unixTimestamp.ToString()));
+//            writeScoreTable(highscoreTable);
+//            return;
+//        }
+//
+//        // load the scoring table from file
+//        string[] substrings;
+//
+//        string fileStr = file.ReadToEnd();
+//        string[] txtlines = fileStr.Split(new char[] { '\n' });
+//        file.Close();
+//
+//        for (int i = 0; i < txtlines.Length && highscoreTable.Count < 5; i++) {
+//            if (txtlines[i] == "") {
+//                continue;
+//            }
+//
+//            substrings = Regex.Split(txtlines[i], " ");
+//            ScoreRow row = new ScoreRow(i, int.Parse(substrings[1]), substrings[0]);
+//
+//            if (i==0)
+//            {
+//                bestScore = int.Parse(substrings[1]);
+//            }
+//
+//            if (row.isBetter(score))
+//            {
+//                if (highscoreTable.Count < 4)
+//                {
+//                    if (i == 0)
+//                    {
+//                        bestScore = score;
+//                    }
+//                    highscoreTable.Add(new ScoreRow(i, score, unixTimestamp.ToString()));
+//                    row.id++;
+//                    highscoreTable.Add(row);
+//                } else {
+//                    row.updateRow(score, unixTimestamp.ToString());
+//                    highscoreTable.Add(row);
+//                }
+//            } else {
+//                highscoreTable.Add(row);
+//            }
+//        }
+//
+//        if (highscoreTable.Count < 5) {
+//            highscoreTable.Add(new ScoreRow(highscoreTable.Count, score, unixTimestamp.ToString()));
+//        }
+//
+//        // update list is necessary
+//        writeScoreTable(highscoreTable);
     }
 
     public int getBestScore() {
